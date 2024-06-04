0332fd31e16f5eaf3dc4c1b237a7274c654b52fc
Swati Jain <jain.swati71@gmail.com>
2024-06-04 18:07:04 +0200

    Completed left side styles of header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,44 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
+        </div>
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+        {/*figures*/}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
       <div className="right-h">right side</div>
     </div>
