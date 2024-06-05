500efb3b47d624629c2a5a376f6d0115f57dfca0
Swati Jain <jain.swati71@gmail.com>
2024-06-05 17:31:52 +0200

    Added auto scroll feature

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
 import "./Header.css";
 import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
   const mobile = window.innerWidth <= 768 ? true : false;
@@ -28,11 +29,57 @@ const Header = () => {
           </div>
         ) : (
           <ul className="header-menu">
-            <li onClick={() => setMenuOpened(false)}>Home</li>
-            <li onClick={() => setMenuOpened(false)}>Programs</li>
-            <li onClick={() => setMenuOpened(false)}>Why us</li>
-            <li onClick={() => setMenuOpened(false)}>Plans</li>
-            <li onClick={() => setMenuOpened(false)}>Testimonials</li>
+            <li>
+              <Link
+                onClick={() => setMenuOpened(false)}
+                activeClass="active"
+                to="home"
+                span={true}
+                smooth={true}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setMenuOpened(false)}
+                to="programs"
+                span={true}
+                smooth={true}
+              >
+                Programs
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setMenuOpened(false)}
+                to="reasons"
+                span={true}
+                smooth={true}
+              >
+                Why us
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setMenuOpened(false)}
+                to="plans"
+                span={true}
+                smooth={true}
+              >
+                Plans
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={() => setMenuOpened(false)}
+                to="testimonials"
+                span={true}
+                smooth={true}
+              >
+                Testimonials
+              </Link>
+            </li>
           </ul>
         )}
       </div>
